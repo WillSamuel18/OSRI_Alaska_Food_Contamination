b59c313e1672176158c3cc8cd15f6cb6992e7637
Will Samuel <wtsamuel@alaska.edu>
2025-04-28 11:40:01 -0800

    Adjusted target dataframe, added new data, and transformed the first dataset (NCCOS clam)

--- Data processing and organization.R
+++ Data processing and organization.R
@@ -20,11 +20,13 @@ library(readxl)
 
 
 #Target dataframe structure:
-#Source             - This is the study or source of the data
+#Data_source        - This is the source of the data
+#Study_name         - This is the study that collected the data
 #Source_siteID      - This is the Site ID as given by the original study/dataset
 #Source_sampleID    - This is the Sample ID as given by the original study/dataset
   #OSRI_siteID        - This is our combined Site ID, to make them consistent
-  #OSRI_sampleID      - This is our combined Sample ID, to make them consistent
+  #OSRI_sampleID      - This is our combined Sample ID, to make them consistent ***
+#Sample_motivation  - Whether the sample was tested for basic biomonitoring,or in response to an oil spill
 #General_location   - The general region where the samples were collected, which are mostly available in the NCCOS and Diver datasets
 #Specific_location  - A description-based naming convention of the specific location, available in the NCCOS and Diver Datasets (e.g., STADESC)
 #Lat                - Latitude of the sample location, in decimal degrees. May need to further refine this depending on the datum/projected coordinate systems. 
@@ -34,19 +36,79 @@ library(readxl)
 #Collection_date    - Date the sample was collected
 #DOY                - Day of Year the sample was collected
 #Collection_time    - The time the sample was collected, if possible
+#Collection_method  - How the sample was obtained (e.g., harvest, found dead, etc.)
   #Species_complex    - The functional group that the species belongs to (e.g., fish, bottom fish, whale, mollusk). This is just for analysis purposes, and my not align with taxonomic classifications. It may need to be refined or dropped in the future.
-#Species            - Common Name of the species
+#Common_name        - Common Name of the species
 #Scientific_name    - Scientific name of the species
+#Genus_latin        - Genus (from scientific name)
+#Species_latin      - Species (from scientific name)
 #Tissue_type        - The type of tissue which the sample was from
-#Method             - The method that was used to test the sample
+#Sample_composition - E.g., a single individual vs a group of pooled individuals
+
+#Sex                - The sex of the animal that was sampled. May not be any data.
+#Analysis_method    - The method that was used to test the sample (e.g., method code)
+#Chem_code          - Potentially universal identifiers of these tests? 
+
 #Parameter          - The chemical that was tested for, we will need to work to make sure these naming conventions are consistent across studies. In the Diver data it's "Analysis_Type"
 #Value              - Measured value of the parameter that was sampled
 #Units              - Units of measurements, may need to transform some of these so they are consistent
-#Detection_limit    - Detection limits, if applicable. I might just add this as a "BLD" in the value column, we'll see
-#Reporting_limit    - If we end up needing this, if it's different from the detection limit
-#Lab_Replicate      - Lab replicates of individual samples
+#Value_standardized - The standardized value, according to the standardized units
+#Units_standardized - In case we need to transform the units
+#Detection_limit    - The value for which the test can detect the target parameter
+#Reporting_limit    - ...
+#Lab_replicate      - Lab replicates of individual samples
+#Qualifier_code     - A lab flag for potential errors, or describing how the value was calculated (if estimated)
 #Lipid_pct          - Percent of lipids within the sample, used to standardize and compare samples properly
+#Total_PAHs         - Total amount of Polycyclic Aromatic Hydrocarbons (PAHs) 
+#Total_LMWAHs       - The total low molecular-weight aromatic hydrocarbons
+#Total_HMWAHs       - The total high molecular-weight aromatic hydrocarbons
+#Lab_ID             - The analytical lab where the samples were tested
 
+
+
+
+df <- data.frame(Data_source = character(),
+                  Study_name = character(),
+                  Source_siteID = character(),
+                  Source_sampleID = character(), 
+                  OSRI_siteID = character(),
+                  OSRI_sampleID = character(),   
+                  Sample_motivation = character(),    
+                  General_location = character(),
+                  Specific_location = character(),
+                  Lat = numeric(),
+                  Long = numeric(),
+                  Year = integer(),
+                  Month = character(),
+                  Collection_date = as.Date(character()),	
+                  DOY = character(),
+                  Collection_time = as.POSIXct(character()),
+                  Collection_method = character(),
+                  Species_complex =character(),
+                  Common_name = character(),
+                  Scientific_name = character(),
+                  Genus_latin = character(),
+                  Species_latin = character(),
+                  Tissue_type = character(),
+                  Sample_composition = character(),
+                  Sex = character(),
+                  Analysis_method = character(),
+                  Chem_code = character(),
+                  Parameter = character(),
+                  Value = numeric(),
+                  Units = character(),
+                  Value_standardized = numeric(),
+                  Units_standardized = character(),
+                  Detection_limit = numeric(),
+                  Reporting_limit = numeric(),
+                  Lab_replicate = numeric(),
+                  Qualifier_code = character(),
+                  Lipid_pct = numeric(),
+                  Total_PAHs = numeric(),
+                  Total_LMWAHs = numeric(),
+                  Total_HMWAHs = numeric(),
+                  Lab_ID = character(),
+                 stringsAsFactors = FALSE)
 
 
 
@@ -81,6 +143,190 @@ str(NCCOS_clam)
 #$ Unit             : chr  "ng/dry g" "ng/dry g" "ng/dry g" "ng/dry g" ...
 #$ X                : logi  NA NA NA NA NA NA ...
 
+
+
+###NCCOS clam data ------------------------------------------------------
+NCCOS_clam_processed <- data.frame(
+  Data_source = rep("NCCOS", nrow(NCCOS_clam)),
+  Study_name = NCCOS_clam$Study,
+  Source_siteID = NCCOS_clam$NST_Site,
+  Source_sampleID = NCCOS_clam$Sample_ID,
+  OSRI_siteID = NA,
+  OSRI_sampleID = NA,
+  Sample_motivation = NA,
+  General_location = NCCOS_clam$General_Location,
+  Specific_location = NCCOS_clam$Specific_Location,
+  Lat = NCCOS_clam$Latitude,
+  Long = NCCOS_clam$Longitude,
+  Year = NCCOS_clam$Fiscal_Year,
+  Month = NA,
+  Collection_date =	NCCOS_clam$Collection_Date,
+  DOY = NA,
+  Collection_time = NA,
+  Collection_method = NA,
+  Species_complex = NCCOS_clam$Matrix,
+  Common_name = NA,
+  Scientific_name = NCCOS_clam$Scientific_Name,
+  Genus_latin = NA,
+  Species_latin = NA,
+  Tissue_type = NA,
+  Sample_composition = NA,
+  Sex = NA,
+  Analysis_method = NCCOS_clam$Method,
+  Chem_code = NA,
+  Parameter = NCCOS_clam$Parameter,
+  Value = NCCOS_clam$Value,
+  Units = NCCOS_clam$Unit,
+  Value_standardized = NA,
+  Units_standardized = NA,
+  Detection_limit = NA,
+  Reporting_limit = NA,
+  Lab_replicate = NA,
+  Qualifier_code = NCCOS_clam$Qualifier,
+  Lipid_pct = NA,
+  Total_PAHs = NA,
+  Total_LMWAHs = NA,
+  Total_HMWAHs = NA,
+  Lab_ID = NA
+  )
+
+  
+
+NCCOS_clam_processed <- NCCOS_clam_processed %>% 
+  mutate(Collection_date = as.Date(Collection_date, format = "%Y-%m-%d"),  
+         Month = month(Collection_date, label = TRUE),
+         DOY = yday(Collection_date),
+           
+         Scientific_name = case_when(
+           Scientific_name == "Mya Arenaria" ~ "Mya arenaria",
+           Scientific_name == "Siliqua Patula" ~ "Siliqua patula",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ),
+         
+         Common_name = case_when(
+           Scientific_name == "Mya arenaria" ~ "Soft-shell clam",
+           Scientific_name == "Siliqua patula" ~ "Pacific razor clam",
+           Scientific_name == "Protothaca staminea" ~ "Pacific littleneck clam",
+           TRUE ~ NA_character_  # Default case if no match is found
+           ),
+         
+         Genus_latin = case_when(
+           Scientific_name == "Mya arenaria" ~ "Mya",
+           Scientific_name == "Siliqua patula" ~ "Siliqua",
+           Scientific_name == "Protothaca staminea" ~ "Protothaca",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ),
+         
+         Genus_latin = case_when(
+           Scientific_name == "Mya arenaria" ~ "arenaria",
+           Scientific_name == "Siliqua arenaria" ~ "arenaria",
+           Scientific_name == "Protothaca staminea" ~ "staminea",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ))
+
+str(NCCOS_clam_processed)
+
+
+
+### NCCOS cockles data -----------------------------------------------------
+
+
+
+### NCCOS fish data -----------------------------------------------------
+
+NEED TO UPDATE TO FISH
+
+NCCOS_clam_processed <- data.frame(
+  Data_source = rep("NCCOS", nrow(NCCOS_clam)),
+  Study_name = NCCOS_clam$Study,
+  Source_siteID = NCCOS_clam$NST_Site,
+  Source_sampleID = NCCOS_clam$Sample_ID,
+  OSRI_siteID = NA,
+  OSRI_sampleID = NA,
+  Sample_motivation = NA,
+  General_location = NCCOS_clam$General_Location,
+  Specific_location = NCCOS_clam$Specific_Location,
+  Lat = NCCOS_clam$Latitude,
+  Long = NCCOS_clam$Longitude,
+  Year = NCCOS_clam$Fiscal_Year,
+  Month = NA,
+  Collection_date =	NCCOS_clam$Collection_Date,
+  DOY = NA,
+  Collection_time = NA,
+  Collection_method = NA,
+  Species_complex = NCCOS_clam$Matrix,
+  Common_name = NA,
+  Scientific_name = NCCOS_clam$Scientific_Name,
+  Genus_latin = NA,
+  Species_latin = NA,
+  Tissue_type = NA,
+  Sample_composition = NA,
+  Sex = NA,
+  Analysis_method = NCCOS_clam$Method,
+  Chem_code = NA,
+  Parameter = NCCOS_clam$Parameter,
+  Value = NCCOS_clam$Value,
+  Units = NCCOS_clam$Unit,
+  Value_standardized = NA,
+  Units_standardized = NA,
+  Detection_limit = NA,
+  Reporting_limit = NA,
+  Lab_replicate = NA,
+  Qualifier_code = NCCOS_clam$Qualifier,
+  Lipid_pct = NA,
+  Total_PAHs = NA,
+  Total_LMWAHs = NA,
+  Total_HMWAHs = NA,
+  Lab_ID = NA
+)
+
+
+
+NCCOS_clam_processed <- NCCOS_clam_processed %>% 
+  mutate(Collection_date = as.Date(Collection_date, format = "%Y-%m-%d"),  
+         Month = month(Collection_date, label = TRUE),
+         DOY = yday(Collection_date),
+         
+         Scientific_name = case_when(
+           Scientific_name == "Mya Arenaria" ~ "Mya arenaria",
+           Scientific_name == "Siliqua Patula" ~ "Siliqua patula",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ),
+         
+         Common_name = case_when(
+           Scientific_name == "Mya arenaria" ~ "Soft-shell clam",
+           Scientific_name == "Siliqua patula" ~ "Pacific razor clam",
+           Scientific_name == "Protothaca staminea" ~ "Pacific littleneck clam",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ),
+         
+         Genus_latin = case_when(
+           Scientific_name == "Mya arenaria" ~ "Mya",
+           Scientific_name == "Siliqua patula" ~ "Siliqua",
+           Scientific_name == "Protothaca staminea" ~ "Protothaca",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ),
+         
+         Genus_latin = case_when(
+           Scientific_name == "Mya arenaria" ~ "arenaria",
+           Scientific_name == "Siliqua arenaria" ~ "arenaria",
+           Scientific_name == "Protothaca staminea" ~ "staminea",
+           TRUE ~ NA_character_  # Default case if no match is found
+         ))
+
+str(NCCOS_clam_processed)
+
+
+### NCCOS flatfish data -----------------------------------------------------
+
+
+### NCCOS mussel data -----------------------------------------------------
+
+
+### NCCOS shrimp data -----------------------------------------------------
+
+
+### NCCOS starfish data -----------------------------------------------------
 
 
 
@@ -210,21 +456,13 @@ str(Diver_data)
 
 
 
-
-
-
-
-
-
-
-
 # Wetzel Data -------------------------------------------------------------
 
 
-Wetzel_fish <- read_excel("Input Data/Wetzel Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Fish")      
-Wetzel_Crustaceans <- read_excel("Input Data/Wetzel Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Crustaceans")      
-Wetzel_Pinnipeds <- read_excel("Input Data/Wetzel Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Pinnipeds")      
-Wetzel_Whale <- read_excel("Input Data/Wetzel Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Whale")      
+Wetzel_fish <- read_excel("Input Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Fish")      
+Wetzel_Crustaceans <- read_excel("Input Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Crustaceans")      
+Wetzel_Pinnipeds <- read_excel("Input Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Pinnipeds")      
+Wetzel_Whale <- read_excel("Input Data/Wetzel Lab_PAHinAKTissues_3_25_25 copy.xlsx", sheet = "Whale")      
 
 View(Wetzel_fish)
 str(Wetzel_fish)
@@ -241,6 +479,132 @@ str(Wetzel_fish)
 
 
 
+
+
+# Stimmelmayr Data --------------------------------------------------------
+
+Stimmelmayr_data <- read_excel("Input Data/Stimmelmayr et al 2018_Seal_MinedTable4.xlsx")      
+
+str(Stimmelmayr_data)
+#tibble [33 x 20] (S3: tbl_df/tbl/data.frame)
+#$ Tissue                   : chr [1:33] "Blubber" "Blubber" "Blubber" "Blubber" ...
+#$ Species                  : chr [1:33] "Spotted seal" "Spotted seal" "Ringed seal" "Ringed seal" ...
+#$ Field identication number: chr [1:33] "N52-2012" "2012-166" "N55-2012" "N55-2012" ...
+#$ Collection site          : chr [1:33] "Shishmaref" "Gambell" "Gambell" "Gambell" ...
+#$ Collection date          : chr [1:33] "41157" "41198" "41225" "41225" ...
+#$ Collection Method        : chr [1:33] "Subsistence harvest" "Subsistence harvest" "Subsistence harvest" "Subsistence harvest" ...
+#$ Sample Motivation        : chr [1:33] "oiling observed" "oiling observed" "oiling observed" "oiling observed" ...
+#$ Sample n                 : num [1:33] 1 1 1 1 3 1 1 1 1 1 ...
+#$ Percent lipid            : chr [1:33] "89" "85" "94" "92" ...
+#$ LOQ                      : chr [1:33] "6.4 ± 7.8" "6.4 ± 7.8" "6.4 ± 7.8" "6.4 ± 7.8" ...
+#$ SUM LMWAHs               : chr [1:33] "35" "48" "12" "25" ...
+#$ SUMHMWAHS                : chr [1:33] "0.6" "0.4" "< LOQ" "< LOQ" ...
+#$ SumPAHs                  : chr [1:33] "36" "48" "12" "25" ...
+#$ Unit                     : chr [1:33] "ng/g" "ng/g" "ng/g" "ng/g" ...
+#$ Basis                    : chr [1:33] "wet weight" "wet weight" "wet weight" "wet weight" ...
+#$ Comments                 : chr [1:33] NA NA "Sample collected from a non-visibly oiled area of seal carcass." "Sample collected from a visibly oiled area of seal carcass." ...
+#$ Analysis method          : chr [1:33] "GC/MS" "GC/MS" "GC/MS" "GC/MS" ...
+#$ QA/QC measures           : chr [1:33] "met" "met" "met" "met" ...
+#$ Lab                      : chr [1:33] "National Marine Fisheries\r\nServices's Northwest Fisheries Science Center in Seattle, Washington" "National Marine Fisheries\r\nServices's Northwest Fisheries Science Center in Seattle, Washington" "National Marine Fisheries\r\nServices's Northwest Fisheries Science Center in Seattle, Washington" "National Marine Fisheries\r\nServices's Northwest Fisheries Science Center in Seattle, Washington" ...
+#$ Source                   : chr [1:33] "Stimmelmayr et al 2018" "Stimmelmayr et al 2018" "Stimmelmayr et al 2018" "Stimmelmayr et al 2018" ...
+
+
+
+
+
+
+# Arnold Data -------------------------------------------------------------
+
+
+Arnold_data <- read_excel("Input Data/Arnold 2006_SelendangAYU_AKDeptHealth_MinedTable4.xlsx")      
+
+str(Arnold_data)
+#tibble [67 x 15] (S3: tbl_df/tbl/data.frame)
+#$ Collection Date   : POSIXct[1:67], format: "2005-06-25" "2005-06-23" "2005-06-23" "2005-06-23" ...
+#$ Species           : chr [1:67] "black chitons" "black chitons" "black chitons" "black chitons" ...
+#$ Location ID       : chr [1:67] "Captains Bay" "Humpy Cove 1" "Humpy Cove 1" "Humpy Cove 1" ...
+#$ Sample ID         : chr [1:67] "CH-CBW01-06-25-05-01" "CH-SMB7-062305-01-rep" "CH-SMB7-062305-01-rep" "CH-SMB7-062305-01-rep" ...
+#$ Replicate         : num [1:67] 0 1 2 3 0 0 0 0 0 0 ...
+#$ Sample Motivation : chr [1:67] "Post Seledang AYU spill assessment" "Post Seledang AYU spill assessment" "Post Seledang AYU spill assessment" "Post Seledang AYU spill assessment" ...
+#$ Sample Composition: chr [1:67] "10-20 individuals pooled" "10-20 individuals pooled" "10-20 individuals pooled" "10-20 individuals pooled" ...
+#$ Total PAHs        : num [1:67] 36 836 791 1882 13.8 ...
+#$ Unit              : chr [1:67] "µg/kg" "µg/kg" "µg/kg" "µg/kg" ...
+#$ Basis             : logi [1:67] NA NA NA NA NA NA ...
+#$ LOQ               : logi [1:67] NA NA NA NA NA NA ...
+#$ Lab               : chr [1:67] "Woods Hole Group Analytical Laboratory in Raynham, Massachusetts" "Woods Hole Group Analytical Laboratory in Raynham, Massachusetts" "Woods Hole Group Analytical Laboratory in Raynham, Massachusetts" "Woods Hole Group Analytical Laboratory in Raynham, Massachusetts" ...
+#$ Analysis Method   : chr [1:67] "USEPA8270c" "USEPA8270c" "USEPA8270c" "USEPA8270c" ...
+#$ Comments          : chr [1:67] "Samples from Unalaska Bay" "Samples from Unalaska Bay" "Samples from Unalaska Bay" "Samples from Unalaska Bay" ...
+#$ Source            : chr [1:67] "Arnold 2006- Public Health Evaluation of Subsistence\r\nResources Collected During 2005\r\n" "Arnold 2006- Public Health Evaluation of Subsistence\r\nResources Collected During 2005\r\n" "Arnold 2006- Public Health Evaluation of Subsistence\r\nResources Collected During 2005\r\n" "Arnold 2006- Public Health Evaluation of Subsistence\r\nResources Collected During 2005\r\n" ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Merge all the dataframes ------------------------------------------------
+
+NCCOS_clam_processed
+
+
+
+
+
+
+
+
+# Storage -----------------------------------------------------------------
+
+NCCOS_clam_processed <- df %>% rename ( 
+  Data_source = 
+    Study_name = 
+    Source_siteID = 
+    Source_sampleID = 
+    OSRI_siteID = 
+    OSRI_sampleID = 
+    Sample_motivation =    
+    General_location = 
+    Specific_location = 
+    Lat = 
+    Long = 
+    Year = 
+    Month = 
+    Collection_date =	
+    DOY = 
+    Collection_time = 
+    Collection_method = 
+    Species_complex =
+    Species = 
+    Scientific_name = 
+    Genus_latin = 
+    Species_latin = 
+    Tissue_type = 
+    Sample_composition = 
+    Sex = 
+    Analysis_method = 
+    Chem_code = 
+    Parameter = 
+    Value = 
+    Units = 
+    Value_standardized = 
+    Units_standardized = 
+    Detection_limit = 
+    Reporting_limit = 
+    Lab_replicate = 
+    Qualifier_code = 
+    Lipid_pct = 
+    Total_PAHs = 
+    Total_LMWAHs = 
+    Total_HMWAHs = 
+    Lab_ID =
+    
+)
 
 
 
